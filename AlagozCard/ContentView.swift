fe3700b6f16e0c8fc9022bf3b4a2321c4b446690
murat alagoz <muratalagoz84@gmail.com>
2019-10-10 23:24:18 +0300

    Initial Commit

---
+++ AlagozCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlagozCard
+//
+//  Created by murat on 10.10.2019.
+//  Copyright © 2019 alagozsoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
